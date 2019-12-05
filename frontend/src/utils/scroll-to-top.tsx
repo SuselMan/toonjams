e8b60ad6f97c80e4ae2582dabb51b6a1196b63f4
hljm <ilyahljm@gmail.com>
2019-12-05 22:28:25 +0300

    Correct comment

--- frontend/src/utils/scroll-to-top.tsx
+++ frontend/src/utils/scroll-to-top.tsx
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
 /**
- * Custom hook which scroll page to top after navigation to another react-router component
+ * Custom hook which scroll page to top after navigation to another react-router link
  */
 export const ScrollToTop = () => {
   const { pathname } = useLocation();
